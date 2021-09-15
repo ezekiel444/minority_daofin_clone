39c7f1afddb5fe80b4d7afedcbe82f0dcc334a31
ezekiel444 <ezekielmatomi588@gmail.com>
2021-09-15 11:31:28 +0200

    add component design ready but no functionality

--- src/components/add/Add.js
+++ src/components/add/Add.js
@@ -6,33 +6,66 @@ export default function Add() {
         <StyleSection>
          <h3>Add DAO Form</h3>
          <main>
-             <form>
-                 <div className="form_group">
-              <label htmlFor="Full-name">Full name</label>   
+             <form>   
+             <div className="name">
+             <label htmlFor="Full-name">Full name</label>   
               <input type="text" id="Full-name" />
-                 </div>
+             </div>
+              
+             <div className="category">
               <label htmlFor="Category">Category</label> 
               <input type="text" id="Category" />
+              </div>
+
+              <div className="date">
               <label htmlFor="Date-found"> Date Found</label>
               <input type="text" id="Date-found" />
+              </div>
+
+              <div className="token-symbol">
               <label htmlFor="Token-symbol">Token symbol </label>  
               <input type="text" id="Token-symbol" />
+              </div>
+
+              <div className="token-name">
               <label htmlFor="Token-name">Governance Token Name</label>   
               <input type="text" id="Token-name" />
+              </div>
+
+              <div className="twitter">
               <label htmlFor="Twitter">Twitter Handle </label> 
               <input type="text" id="Twitter" />  
+              </div>
+
+              <div className="discord">
               <label htmlFor="Discord">Discord </label>   
               <input type="text" id="Discord" />
+              </div>
+
+              <div className="website">
               <label htmlFor="Website">Website</label>
               <input type="text" id="Websit" />
+              </div>
+
+              <div className="tvl">
               <label htmlFor="TVL">TVL</label>
               <input type="text" id="TVL" />
+              </div>
+
+              <div className="block-chain">
               <label htmlFor="Block-chain">Block-chain</label>
               <input type="text" id="Block-chain" />
+              </div>
+
+              <div className="head-quarter">
               <label htmlFor="Head-quarters">Head-quarters</label>
               <input type="text" id="Head-quarters" />
+              </div>
+
+              <div className="description">
               <label htmlFor="Discription">Discription</label>
               <input type="text" id="Discription" placeholder="Brief Description" />
+              </div>
               <button id="Submit">Submit</button>
 
              </form>
--- src/components/add/styleAdd.js
+++ src/components/add/styleAdd.js
@@ -4,7 +4,87 @@ import styled from "styled-components";
 const StyleSection = styled.section`
 height: 100vh;
 width: 85vw;
-/* background-color: red; */
+
+
+form{
+    width: 55vw;
+    height: 75vh;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+    grid-gap: 20px;
+    grid-template-areas: "name name category"
+                         "date token-symbol token-name"
+                         "twitter discord website"
+                         "tvl block-chain head-quarter"
+                         "description description description";
+
+          
+    div{
+        box-sizing: border-box;
+        height: 65px;  
+    }
+
+    input{
+     border: none;
+    display: block;
+    width: 100%;
+    height: 100%;
+    background: var(--main-color);
+    color: var(--border);
+    font: 1.2em "Fira Sans", sans-serif;
+}
+
+input::placeholder{
+color: #ceacac;
+}
+                         
+
+.name{
+    grid-area: name;
+    
+}
+
+.category{
+    grid-area: category;
+    display: block;
+
+}
+.date{
+    grid-area:date;
+}
+.token-symbol{
+    grid-area:token-symbol;
+}
+.token-name{
+    grid-area:token-name;
+}
+.twitter{
+    grid-area:twitter;
+}
+.discord{
+    grid-area:discord;
+}
+.head-quarter{
+    grid-area:head-quarter;
+}
+.description{
+    grid-area:description;
+}
+
+}
+
+button{
+    background: var(--main-color);
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin-left: 5px;
+}
 
 `
 
