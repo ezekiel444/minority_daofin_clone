aaca06380da401df692300d459e3dd2855ebbe56
ezekiel444 <ezekielmatomi588@gmail.com>
2021-09-20 18:36:35 +0200

    search bar

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -1,16 +1,25 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import StyleSearchResult from './StyleSearch';
 
 export default function SearchBar({data}) {
-    const [inputText, setInputText] = useState('')
+    const [inputText, setInputText] = useState(null)
     const {daofinds} = data
-   const searchData = daofinds.filter(data=>data.name.toLowerCase().includes(inputText.toLowerCase()))
+   const searchData = daofinds.filter(data=>{
+       if(!inputText) {
+           return null
+       }
+      return data.name.toLowerCase().includes(inputText.toLowerCase())
+   })
 
-   console.log(searchData);
-
-    return (
-        <div>
+   return (
+        <StyleSearchResult>
              <input type="text" placeholder="Search Dao" name="text" value={inputText} onChange={(e)=>setInputText(e.target.value)} />
-
-        </div>
+            <div className="searchResult">
+               {searchData.map(search=>{
+                   return <Link to={`/detail/${search.id}`}><p>{search.name}</p></Link>
+               })}
+            </div>
+        </StyleSearchResult>
     )
 }
---
+++ src/components/search/StyleSearch.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+
+
+const StyleSearchResult = styled.main`
+a{
+    color: var(--border);
+font-size: 1.1em;
+text-decoration: none;
+}
+.searchResult{
+ 
+    /* background:var(--b-primary); */
+   
+}
+
+
+`
+
+export default StyleSearchResult;
